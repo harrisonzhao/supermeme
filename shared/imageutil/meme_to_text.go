53a16126d0ef444adfd49007f0f93537188dc019
Harrison Zhao <roarofpie@gmail.com>
2016-12-28 14:28:54 -0500

    fixed GetTextFromMeme function

--- shared/imageutil/meme_to_text.go
+++ shared/imageutil/meme_to_text.go
@@ -2,6 +2,7 @@ package imageutil
 
 import (
 	"errors"
+	"github.com/disintegration/imaging"
 	"github.com/otiai10/gosseract"
 	"image"
 	"image/color"
@@ -15,59 +16,56 @@ type ImageSet interface {
 	Set(x, y int, c color.Color)
 }
 
-// This function sometimes does not successfully separate top from bottom text
-// Need to cut image in half and save each half separately to get top and bottom
 func GetTextFromMeme(img image.Image) (topText, botText string, err error) {
 	b := img.Bounds()
-	topText = ""
-	botText = ""
-	if cimg, ok := img.(ImageSet); ok {
-		for x := b.Min.X; x < b.Max.X; x++ {
-			for y := b.Min.Y; y < b.Max.Y; y++ {
-				oldPixel := img.At(x, y)
-				if color.Gray16Model.Convert(oldPixel) != color.White {
-					cimg.Set(x, y, color.Black)
+	var texts [2]string
+	rects := [2]image.Rectangle{
+		image.Rect(b.Min.X, b.Min.Y, b.Max.X, b.Max.Y/2),
+		image.Rect(b.Min.X, b.Max.Y/2, b.Max.X, b.Max.Y),
+	}
+	for i, rect := range rects {
+		cropped := imaging.Crop(img, rect)
+		croppedImg := cropped.SubImage(cropped.Rect)
+		if cimg, ok := croppedImg.(ImageSet); ok {
+			cb := croppedImg.Bounds()
+			for x := cb.Min.X; x < cb.Max.X; x++ {
+				for y := cb.Min.Y; y < cb.Max.Y; y++ {
+					oldPixel := croppedImg.At(x, y)
+					if color.Gray16Model.Convert(oldPixel) != color.White {
+						cimg.Set(x, y, color.Black)
+					}
 				}
 			}
-		}
-		var file *os.File
-		file, err = ioutil.TempFile(os.TempDir(), "prefix")
-		if err != nil {
-			return
-		}
-		defer os.Remove(file.Name())
-		if err = png.Encode(file, img); err != nil {
-			return
-		}
-		file.Sync()
-		out := gosseract.Must(gosseract.Params{
-			Src:       file.Name(),
-			Languages: "eng",
-		})
-		topText, botText = getTopBotText(out)
-	} else {
-		err = errors.New("Image type does not support set method")
-	}
-	return
-}
-
-func getTopBotText(txt string) (topText, botText string) {
-	stringPieces := strings.Split(txt, "\n")
-	isTop := true
-	for i, piece := range stringPieces {
-		if len(piece) == 0 || len(strings.TrimSpace(piece)) == 0 {
-			isTop = false
-			continue
-		}
-		str := piece
-		if i != len(stringPieces)-1 {
-			str += "\n"
-		}
-		if isTop {
-			topText += str
+			var file *os.File
+			file, err = ioutil.TempFile(os.TempDir(), "prefix")
+			if err != nil {
+				return
+			}
+			defer os.Remove(file.Name())
+			if err = png.Encode(file, croppedImg); err != nil {
+				return
+			}
+			file.Sync()
+			text := gosseract.Must(gosseract.Params{
+				Src:       file.Name(),
+				Languages: "eng",
+			})
+			stringPieces := strings.Split(text, "\n")
+			for j, piece := range stringPieces {
+				if len(piece) == 0 || len(strings.TrimSpace(piece)) == 0 {
+					continue
+				}
+				if j != 0 {
+					texts[i] += "\n"
+				}
+				texts[i] += piece
+			}
 		} else {
-			botText += str
+			err = errors.New("Image type does not support set method")
+			return
 		}
 	}
+	topText = texts[0]
+	botText = texts[1]
 	return
 }
