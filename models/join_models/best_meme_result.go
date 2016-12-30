01b9b260fca87fb019379255228ce64497f91220
Harrison Zhao <roarofpie@gmail.com>
2016-12-29 20:39:47 -0500

    fixed best meme result query

--- models/join_models/best_meme_result.go
+++ models/join_models/best_meme_result.go
@@ -14,12 +14,16 @@ func BestMemeResultsByKeywords(db models.XODB, keywords []string) (*BestMemeResu
 	if len(keywords) == 0 {
 		return nil, nil
 	}
+	keywordArgs := make([]interface{}, len(keywords))
+	for i := range keywords {
+		keywordArgs[i] = keywords[i]
+	}
 	sql, args, err := squirrel.Select(
 		"m.id",
 		"COUNT(mk.keyword) / COALESCE(NULLIF(m.num_keywords, 0), 1000) * LOG(GREATEST(m.net_ups, 1)) as score").
 		From("meme m").
 		Join("meme_keyword mk ON mk.meme_id = m.id").
-		Where("mk.keyword IN ("+squirrel.Placeholders(len(keywords))+")", keywords).
+		Where("mk.keyword IN ("+squirrel.Placeholders(len(keywords))+")", keywordArgs...).
 		GroupBy("m.id").
 		OrderBy("score DESC").
 		Limit(1).
@@ -27,7 +31,7 @@ func BestMemeResultsByKeywords(db models.XODB, keywords []string) (*BestMemeResu
 	if err != nil {
 		return nil, err
 	}
-	rows, err := db.Query(sql, args)
+	rows, err := db.Query(sql, args...)
 	if err != nil {
 		return nil, err
 	}
