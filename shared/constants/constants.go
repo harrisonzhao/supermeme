1457c657188b74a3f739755c03616e13f0462600
harrisonzhao <roarofpie@gmail.com>
2017-02-22 06:28:43 +0000

    changed domain to answerme.me

--- shared/constants/constants.go
+++ shared/constants/constants.go
@@ -1,7 +1,7 @@
 package constants
 
 const (
-	Address             = "https://catchupbot.com"
+	Address             = "https://answerme.me"
 	DbName              = "alpha"
 	FbPageAccessToken   = "EAAYIJqj8spoBALW63WRKqUU88SDAYwSDpobEfUHScfT8Jbet1yMefiZB1foHUpYh5TsZCzjKbDpFZAe4RiL6QNkuBZBsURMAS9YyYvn23ivNr1xkcAZAYP1m5nRSYy93NT1ZBIRvbnctBHUeypuZC5Sgc9ubgOknMRqfQeAzdujZBwZDZD"
 	FbVerificationToken = "this_is_a_verification_token_made_by_harrison"
